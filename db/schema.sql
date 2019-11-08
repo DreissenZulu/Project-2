bcff5e155a9e99cbc00955e63ecb8f4863d1e40c
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-08 13:10:51 -0500

    Added createdAt field

--- db/schema.sql
+++ db/schema.sql
@@ -4,18 +4,40 @@ CREATE DATABASE socialAppDB;
 
 USE socialAppDB;
 
+-- 64 character password to accomodate password encryption
 CREATE TABLE userInfo (
-	id INT auto_increment PRIMARY KEY,
+    id int auto_increment primary key,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     userName VARCHAR(30) NOT NULL,
-    password VARCHAR(256) NOT NULL,
+    password VARCHAR(64) NOT NULL,
     first_name varchar(30) NOT NULL,
     last_name varchar(30)
 );
 
+-- mbid refers to a song or album the comment is associated with, pulled from the last fm API
 create table allComments (
-	id int auto_increment primary key,
+    id int auto_increment primary key,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     commentContent varchar(300) not null,
     user_id int not null,
     likes int default 0 not null,
-    mbid varchar(30) not null
+    playlist_id varchar(30) not null
+);
+
+create table playlist (
+    id int auto_increment primary key,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    playlist_name varchar(30) not null,
+    playlist_genre varchar(30) not null,
+    likes int default 0 not null,
+    user_id int not null
+);
+
+-- mbid and song are not unique as a song can be part of multiple playlists
+create table playlistSongs (
+    id int auto_increment primary key,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    mbid varchar(50) not null,
+    song varchar(30) not null,
+    playlist_id int not null
 );
