6cdbfbdb57e9e874b4acaf693cf20f3281ecfb80
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-11 15:21:54 -0500

    Made orm calls asynchronous

--- config/orm.js
+++ config/orm.js
@@ -3,21 +3,21 @@ const connection = require("./connection.js");
 
 // Put functions that interact with the server directly here.
 const orm = {
-    selectData: (tables, columns, condition, resolve) => {
+    selectData: async (tables, columns, condition, resolve) => {
         let queryString = `SELECT ${columns} FROM ${tables} ${condition};`;
         await connection.query(queryString, function (err, res) {
             if (err) throw err;
             resolve(res);
         });
     },
-    insertData: (table, columns, vals, resolve) => {
+    insertData: async (table, columns, vals, resolve) => {
         let queryString = `INSERT INTO ${table} (${columns}) VALUES (${vals});`;
         await connection.query(queryString, function (err, res) {
             if (err) throw err;
             resolve(res);
         });
     },
-    updateData: (table, updateVal, condition, resolve) => {
+    updateData: async (table, updateVal, condition, resolve) => {
         let queryString = `UPDATE ${table} SET ${updateVal} WHERE ${condition};`;
         await connection.query(queryString, function (err, res) {
             if (err) throw err;
