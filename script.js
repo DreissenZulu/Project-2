4a9880fc66cf8c483a6b586e11c5f56862607d48
Billy Trang <trang.billy@live.fr>
2019-11-11 11:26:08 -0500

    nav-bar reusable on everypage

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ if ($(window).width() < 480) {
     $('body > header > nav > ul').css('margin', '')
 }
 });
+
+var mq = window.matchMedia("(max-width: 480px)");
+if (mq.matches) {
+        $('nav').addClass('row')
+        $('body > header > nav > a').addClass('col-sm-12')
+        $('body > header > nav > ul').css('margin', '0 auto')
+} else {
+        $('nav').removeClass('row')
+        $('body > header > nav > a').removeClass('col-sm-12')
+        $('body > header > nav > ul').css('margin', '')
+}
+
+$(document).ready(function () {
+    $("#nav-placeholder").load("nav.html");
+});
