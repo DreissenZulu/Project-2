f5029e80a304e28868b736fd3656288ec011e386
Billy Trang <trang.billy@live.fr>
2019-11-11 10:31:47 -0500

    navbar responsive

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ $(".added").click(function () {
         $(".alert").addClass("hide");
     }, 2500);
 });
+
+
+$(window).resize(function () {
+if ($(window).width() < 480) {
+    $('nav').addClass('row')
+    $('body > header > nav > a').addClass('col-sm-12')
+    $('body > header > nav > ul').css('margin', '0 auto')
+} else {
+    $('nav').removeClass('row')
+    $('body > header > nav > a').removeClass('col-sm-12')
+    $('body > header > nav > ul').css('margin', '')
+}
+});
