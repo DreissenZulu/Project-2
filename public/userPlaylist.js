9c4c6fa3fd1728c3396e5d8ada2692dcf3514c8c
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-13 15:57:43 -0500

    Created JS file for playlist

---
+++ public/userPlaylist.js
@@ -0,0 +1,65 @@
+function populatePlaylist(id) {
+    return $.ajax({
+        url: `/playlist/${id}`,
+        method: "GET",
+        success: (data) => {
+            let listNum = 0;
+            let dateCreated = data.playlist[0].createdAt.split(/[a-zA-Z]/g)[0]
+            console.log(dateCreated)
+            $(".playlist-name").first().text(data.playlist[0].playlist_name);
+            $(".user").first().text(data.playlist[0].userName);
+            $(".genre").first().text(data.playlist[0].playlist_genre);
+            $(".create-date").first().text(dateCreated);
+            $(".playlist-description").first().text(data.playlist[0].playlist_description);
+            console.log(data.songs[1].song);
+            for(song of data.songs) {
+                listNum++;
+                $("#songList").append(`
+                    <tr>
+                    <th scope="row">${listNum}</th>
+                    <td>${song.song}</td>
+                    <td>${song.artist}</td>
+                    <td align="center"><a href="#" data-toggle="modal" data-target="#myModal"><img src="assets/images/clear.png" class="remove"></a></td>
+                    </tr>
+                `)
+                console.log(song);
+            }
+        }
+    });
+}
+
+$(document).ready(function () {
+    let playlistQuery = self.location.search.split(/={1}/g)
+    populatePlaylist(playlistQuery[1]);
+    if (localStorage.getItem("currUser")) {
+        currUser = JSON.parse(localStorage.getItem("currUser"))
+    } else {
+        currUser = {};
+    }
+    $("#nav-placeholder").load("nav.html", () => {
+        if (currUser.confirmLogin) {
+            $("#navItems").html(`
+            <li class="nav-item"><a class="nav-link" href="/dashboard">My Page</a></li>
+            <li class="nav-item"><a class="nav-link" id="logOut" href="#">Log Out</a></li>
+            `)
+            $("#logOut").click(() => {
+                event.stopPropagation();
+                if (currUser.confirmLogin) {
+                    submitLogOut();
+                }
+            })
+        } else {
+            $("#navItems").html(`
+            <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="/signup">Sign Up</a></li>
+            <li class="nav-item"><a class="nav-link" href="/login">Log in</a></li>
+            `)
+        }
+        $("#logOut").click(() => {
+    console.log("Clicked!")
+    if (currUser.confirmLogin) {
+        submitLogOut();
+    }
+})
+    });
+})
