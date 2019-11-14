7a4bf1fecec506c354585e2648cbe520f4e390e0
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-14 13:34:01 -0500

    Moved dashboard specific code to new js file

---
+++ public/scripts/dashboard.js
@@ -0,0 +1,42 @@
+function getMyPlaylists(id) {
+    return $.ajax({
+        url: `/playlists/${id}`,
+        method: "GET",
+        success: (playlists) => {
+            for (list of playlists) {
+                $("#myPlaylists").append(`
+                    <a class="nav-link" href="/playlist?=${list.id}">${list.playlist_name}</a>
+                `)
+            }
+        }
+    })
+}
+
+function createPlaylist(data) {
+    return $.ajax({
+        url: "/api/playlists",
+        data: data,
+        method: "POST",
+        success: () => {
+            location.reload();
+        }
+    })
+}
+
+$("#playlistCreate").click(() => {
+    if (currUser.userName == "") {
+        console.log("Please log in to create a playlist.");
+        return;
+    }
+    let playlistInfo = {
+        creatorID: currUser.id,
+        playlistName: $("#newPlaylistName").val().trim(),
+        playlistDesc: $("#newPlaylistDesc").val().trim(),
+        playlistGenre: $("#newPlaylistGenre").val()
+    }
+    createPlaylist(playlistInfo);
+})
+
+$(document).ready(function () {
+    getMyPlaylists(currUser.id);
+})
