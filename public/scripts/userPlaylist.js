f3a3787d858e804f8d3e7a416dd8d884f88f043e
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-15 12:05:22 -0500

    Playlist page now allows users to add songs to their own playlist if the playlist doesn't belong to them

--- public/scripts/userPlaylist.js
+++ public/scripts/userPlaylist.js
@@ -1,3 +1,5 @@
+let selectSong;
+
 function populatePlaylist(id) {
     return $.ajax({
         url: `/playlist/${id}`,
@@ -22,6 +24,15 @@ function populatePlaylist(id) {
                         </tr>
                     `)
                 }
+                $(".modal-content").first().html(`
+                    <div class="modal-body">
+                    <p id="modal-playlist">Do you really want to remove <strong> "Your Song" </strong>from your playlist ?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-success rounded-0" data-dismiss="modal" id="confirm">Confirm</button>
+                        <button class="btn btn-danger rounded-0" data-dismiss="modal">Cancel</button>
+                    </div>
+                `)
                 // Removal function is only available to the user if they're logged in AND the playlist belongs to them
                 $(".song-remove").click((event) => {
                     event.stopPropagation();
@@ -30,26 +41,87 @@ function populatePlaylist(id) {
                     stageSong(songID, songText);
                     $("#myModal").modal()
                 })
+                $("#confirm").click((event) => {
+                    let songID = $(event.currentTarget).attr('value');
+                    let playlistID = self.location.search.split(/={1}/g)[1]
+                    removeSong(playlistID, songID);
+                })
             } else {
                 for (song of data.songs) {
                     listNum++;
                     $("#songList").append(`
                         <tr>
                         <th scope="row">${listNum}</th>
-                        <td><a href="/track?=${song.artist}?=${song.song}">${song.song}</a></td>
-                        <td>${song.artist}</td>
-                        <td></td>
+                        <td class="title"><a href="/track?=${song.artist}?=${song.song}">${song.song}</a></td>
+                        <td class="artist">${song.artist}</td>
+                        <td align="center"><a href="#" class="add-song"><img src="assets/images/add.png" class="add"></a></td>
                         </tr>
                     `)
                 }
+                $(".modal-content").first().html(`
+                    <div class="modal-header">
+                    <h1>Add to your playlist</h1>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                            <div class="form-group">
+                                <div class="list-group">
+                                    <a href="/login" style="margin: auto;">Log in to add this song to your playlist!</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-danger rounded-0" data-dismiss="modal">Cancel</button>
+                    </div>
+                `)
+                $(".add-song").click((event) => {
+                    event.stopPropagation();
+                    selectSong = {
+                        songTitle: $(event.currentTarget).parent().siblings('.title').text(),
+                        artistName: $(event.currentTarget).parent().siblings('.artist').text()
+                    }
+                    $("#myModal").modal()
+                })
+                getMyPlaylists(currUser.id);
             }
         }
     });
 }
 
+function getMyPlaylists(id) {
+    return $.ajax({
+        url: `/playlists/${id}`,
+        method: "GET",
+        success: (playlists) => {
+            $(".list-group").first().html("")
+            for (list of playlists) {
+                $(".list-group").first().append(`
+                <a href="#" class="added list-group-item list-group-item-action" id="${list.id}">${list.playlist_name}</a>
+                `)
+            }
+            $(".added").click((event) => {
+                let playlistID = $(event.currentTarget).attr('id');
+                addSong(playlistID, selectSong)
+            })
+        }
+    })
+}
+
 function stageSong(id, title) {
     $("#modal-playlist").html(`Do you really want to remove <strong> ${title} </strong>from your playlist ?`)
     $("#confirm").attr('value', id);
+}
+
+function addSong(playlistID, songInfo) {
+    return $.ajax({
+        url: `/api/playlists/${playlistID}`,
+        data: songInfo,
+        method: "POST",
+        success: () => {
+            $("#myModal").modal('hide')
+        }
+    });
 }
 
 function removeSong(playlistID, songID) {
@@ -64,12 +136,6 @@ function removeSong(playlistID, songID) {
         }
     });
 }
-
-$("#confirm").click((event) => {
-    let songID = $(event.currentTarget).attr('value');
-    let playlistID = self.location.search.split(/={1}/g)[1]
-    removeSong(playlistID, songID);
-})
 
 $(document).ready(function () {
     let playlistQuery = self.location.search.split(/={1}/g)
