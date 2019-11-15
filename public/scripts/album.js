4f811264ba28adaff4463bb6409ca1d0162dd84d
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-11-14 21:50:47 -0500

    Users can now add songs to their playlists via an artist's album page

--- public/scripts/album.js
+++ public/scripts/album.js
@@ -1,3 +1,5 @@
+let selectSong;
+
 async function getAlbumInfo(artist, album) {
     await $.ajax({
         url: `/lastfm/${artist}/${album}`,
@@ -6,18 +8,18 @@ async function getAlbumInfo(artist, album) {
         let albumInfo = response.album;
         let albumImage = albumInfo.image[3];
         let albumGenre = albumInfo.tags.tag[0].name == 'albums I own' ? albumInfo.tags.tag[1].name : albumInfo.tags.tag[0].name;
-        let albumPublished = albumInfo.wiki.published.split(",")[0]
+        let albumPublished = albumInfo.wiki ? albumInfo.wiki.published.split(",")[0] : "Unknown";
+        let albumSummary = albumInfo.wiki ? albumInfo.wiki.summary : "No summary available.";
         let currTrack = 0;
 
         $("title").text(`${albumInfo.name} - ${albumInfo.artist}`)
-
         $(".album-cover").first().attr('src', Object.values(albumImage)[0])
         $("#albumInfo").html(`
             <h1 class="album-name">${albumInfo.name}</h1>
             <h3 class="artist-name">by <i class="artist">${albumInfo.artist}</i></h3>
             <h4 class="genre">${albumGenre}</h4>
             <h4 class="release-date">${albumPublished}</h4>
-            <p class="song-description">${albumInfo.wiki.summary}</p>
+            <p class="song-description">${albumSummary}</p>
         `)
 
         for (track of Object.values(albumInfo.tracks)[0]) {
@@ -30,10 +32,18 @@ async function getAlbumInfo(artist, album) {
                 <th scope="row">${currTrack}</th>
                 <td>${track.name}</td>
                 <td>${trackMinutes}:${trackSeconds}</td>
-                <td align="center"><a href="#" data-toggle="modal" data-target="#myModal"><img src="assets/images/add.png" class="add"></a></td>
+                <td align="center"><a href="#" class="add-song"><img src="assets/images/add.png" class="add"></a></td>
                 </tr>
             `)
         }
+        $(".add-song").click((event) => {
+            event.stopPropagation();
+            selectSong = {
+                songTitle: $(event.currentTarget).parent().siblings('td').html(),
+                artistName: $(".artist").first().text()
+            }
+            $("#myModal").modal()
+        })
     })
 }
 
@@ -45,11 +55,26 @@ function getMyPlaylists(id) {
             $(".list-group").first().html("")
             for (list of playlists) {
                 $(".list-group").first().append(`
-                <a href="#" class="added list-group-item list-group-item-action">${list.playlist_name}</a>
+                <a href="#" class="added list-group-item list-group-item-action" id="${list.id}">${list.playlist_name}</a>
                 `)
             }
+            $(".added").click((event) => {
+                let playlistID = $(event.currentTarget).attr('id');
+                addSong(playlistID, selectSong)
+            })
         }
     })
+}
+
+function addSong(playlistID, songInfo) {
+    return $.ajax({
+        url: `/api/playlists/${playlistID}`,
+        data: songInfo,
+        method: "POST",
+        success: () => {
+            $("#myModal").modal('hide')
+        }
+    });
 }
 
 $(document).ready(() => {
